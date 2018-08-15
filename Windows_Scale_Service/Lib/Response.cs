47f861f6a52b27b0dc316a0caba4b2fcc5f9241b
iheng <techitao.henry@gmail.com>
2018-08-15 13:46:21 -0700

    add scale model options

---
+++ Windows_Scale_Service/Lib/Response.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Ports;
+
+namespace ScaleService.Scale_Models
+{
+    public class Scale_Result
+    {
+        public string UnitOfMesure { get; set; } = "kg";
+        public string Weight { get; set; }
+        public string Scale_Model { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string Scale_Timeout { get; set; } = "300";
+    }
+}
