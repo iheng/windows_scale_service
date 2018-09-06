adde012df8381268f3a378d02addfeeafa032b2d
iheng <techitao.henry@gmail.com>
2018-09-06 13:13:46 -0700

    change directory name

---
+++ Scale_Service/ScaleService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using ScaleService.Lib;
+using System.ServiceModel.Web;
+
+namespace ScaleService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IScaleService
+    {
+        // TODO: Add your service operations here
+        [OperationContract]
+        [WebGet]
+        List<Balance_Result> Get_Weight();
+
+        [OperationContract]
+        [WebGet]
+        List<Ports> Get_Available_Ports();
+
+        [OperationContract]
+        [WebInvoke(Method ="POST")]
+        void Set_Port(string Port_Name);
+    }
+   
+
+}
---
+++ Scale_Service/ScaleWindowsService.cs
@@ -0,0 +1,60 @@
+﻿using System.ServiceModel;
+using System.ServiceProcess;
+using log4net;
+using ScaleService.Scale_Models;
+using System.Threading;
+
+namespace ScaleService
+{
+
+    public class ScaleWindowsService : ServiceBase
+    {
+        
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public ServiceHost serviceHost = null;
+
+        public ScaleWindowsService()
+        {
+            // Name the Windows Service
+            ServiceName = "ScaleWindowsService";
+
+        }
+        public static void Main()
+        {           
+            Run(new ScaleWindowsService());
+        }
+        // Start the Windows service.
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            // Create a ServiceHost for the BreckNell_335 type and 
+            // provide the base address.
+            serviceHost = new ServiceHost(typeof(Scale));
+
+            // Open the ServiceHostBase to create listeners and start 
+            // listening for messages.
+            log.Info("Program Starting");
+            serviceHost.Open();
+            Brecknell._M335.Open_Port();
+
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {        
+                serviceHost.Close();
+                serviceHost = null;
+                Brecknell._M335.Close_Port();
+                Thread.Sleep(100);
+
+            }
+            log.Info("Program exist");
+        }
+    }
+        
+}
